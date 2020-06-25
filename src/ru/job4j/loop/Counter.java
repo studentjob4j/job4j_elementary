0775082858a39f70508992e22c7d960897b026b8
evgenshegai <evgeniishegai@yandex.ru>
2020-06-25 12:42:29 +1000

    5.1. Подсчет суммы чётных чисел в диапазоне [#304373]

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -28,7 +28,7 @@ public class Counter {
 
         System.out.println(sumByEven(0, 10));
         System.out.println(sumByEven(3, 8));
-        System.out.println(sumByEven(1, 1));
+
     }
 
 }
---
+++ test/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.loop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class CounterTest {
+
+    @Test
+    public void sum() {
+      int result = Counter.sum(0, 10);
+      int except = 55;
+        Assert.assertThat(result, is(except));
+    }
+
+    @Test
+    public void sumByEvenFrom0To10() {
+        int result = Counter.sumByEven(0, 10);
+        int except = 30;
+        Assert.assertThat(result, is(except));
+    }
+
+
+    @Test
+    public void sumByEvenFrom0To5() {
+        int result = Counter.sumByEven(0, 5);
+        int except = 6;
+        Assert.assertThat(result, is(except));
+    }
+}
