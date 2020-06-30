255089bd6333d252b20d1612fd608b6678802338
evgenshegai <evgeniishegai@yandex.ru>
2020-07-01 02:16:03 +1000

    1.4. Вызов метод с возвращаемым типом. [#304462]

---
+++ src/ru/job4j/oop/DummyDic.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class DummyDic {
+
+    public String engToRus(String eng) {
+        return "Неизвестное слово " + eng;
+    }
+
+    public static void main(String[] args) {
+        DummyDic dic = new DummyDic();
+        System.out.println(dic.engToRus("Hi"));
+    }
+}
