6b40c8e0c17d4ac6e98e9d1db8756cb811362da5
evgenshegai <evgeniishegai@yandex.ru>
2020-07-01 11:33:55 +1000

    1.7. Состояние объекта. [#304463]

---
+++ src/ru/job4j/oop/Battery.java
@@ -0,0 +1,21 @@
+package ru.job4j.oop;
+
+public class Battery {
+
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public void exchange(Battery another) {
+        another.load = another.load + this.load;
+        System.out.println("Полная зарядка батареи - " + another.load);
+    }
+
+    public static void main(String[] args) {
+        Battery battery = new Battery(30);
+        Battery battery2 = new Battery(50);
+        battery2.exchange(battery);
+    }
+}
